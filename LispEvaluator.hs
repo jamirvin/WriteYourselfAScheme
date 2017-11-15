0ce8cca499de192d9270d1a9ae47788053e35b52
James Irvin <12jdirvin@gmail.com>
2017-11-15 01:01:39 -0500

    Fix last commit

---
+++ LispEvaluator.hs
@@ -0,0 +1,8 @@
+module LispEvaluator where
+import           LispParser
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _)   = val
+eval (List [Atom "quote", val]) = val
